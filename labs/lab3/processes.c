932f55621739ccf2711f52ff04a1e077482cfa17
polocto <paul.sade@live.fr>
2021-10-14 18:10:03 +0200

    parallelisme

--- labs/lab3/processes.c
+++ labs/lab3/processes.c
@@ -19,16 +19,23 @@ clock_t times(struct tms *buf);
 #define KEY5 4571
 #define KEY6 4572
 #define PERMS 0660
+#define REPEAT 10000
+
+int soustraction(int a, int b);
+int somme(int a, int b);
+int division(int a, int b);
+int multiplication(int a, int b);
 
 int main(int argc, char **argv)
 {
     struct tms start, end;
     struct rusage rstart, rend;
     int i;
+    int final;
 
     times(&start);
     getrusage(RUSAGE_SELF, &rstart);
-    for (i = 0; i < 10000; i++)
+    for (i = 0; i < REPEAT; i++)
     {
 
         int id1, id2, id3, idFlag1, idFlag2, idFlag3;
@@ -66,60 +73,67 @@ int main(int argc, char **argv)
             {
                 if (fork() == 0)
                 {
-                    int a = 25, b = 2;
-                    *ptr1 = a + b;
+                    int a = 2, b = 3;
+                    *ptr1 = a + b; // => 5
                     *flag1 = 1;
                     exit(0);
                 }
                 else
                 {
 
-                    int c = 3, d = 7;
-                    *ptr2 = c * d;
+                    int c = 6, d = 2;
+                    *ptr2 = c * d; // => 12
                     *flag2 = 1;
                     exit(0);
                 }
             }
             else
             {
-                int e = 5;
+                int e = 1;
                 int f = 10;
-                *ptr3 = e - f;
+                *ptr3 = e - f; // => -9
                 *flag3 = 1;
                 exit(0);
             }
         }
         else
         {
-            int g = 2;
-            int h = 1;
-            int result = g + h;
+            int g = 21;
+            int h = 12;
+            int result = g + h; // => 33
 
             while (*flag1 == 0 || *flag2 == 0 || *flag3 == 0)
-                ;
+                wait(NULL);
 
-            // printf("a : %d , b : %d\n",a,b);
-            // printf("*ptr1 : %d\n", *ptr1);
-            int Total = (*ptr1) - (*ptr2) / (*ptr3) + result;
-            // printf("%d - %d / %d + %d = %d\n", *ptr1, *ptr2, *ptr3, result, Total); //display results
-            shmdt(ptr1);
-            shmdt(ptr2);
-            shmdt(ptr3);
-            shmdt(flag1);
-            shmdt(flag2);
-            shmdt(flag3);
-            
-            shmctl(id2, IPC_RMID, NULL); // mark ptr to be destroy from the share memory
-            shmctl(id1, IPC_RMID, NULL); // mark ptr to be destroy from the share memory
-            shmctl(id3, IPC_RMID, NULL);
-            shmctl(idFlag2, IPC_RMID, NULL); // mark ptr to be destroy from the share memory
-            shmctl(idFlag1, IPC_RMID, NULL); // mark ptr to be destroy from the share memory
-            shmctl(idFlag3, IPC_RMID, NULL);
+            if(fork()==0){
+                *ptr1 = result + (*ptr1); // => 38
+                exit(0);
+            }
+            else
+            {
+
+                int Total = (*ptr2) / (*ptr3);
+                wait(NULL);
+                final = (*ptr1) - Total;
+                shmdt(ptr1);
+                shmdt(ptr2);
+                shmdt(ptr3);
+                shmdt(flag1);
+                shmdt(flag2);
+                shmdt(flag3);
+                
+                shmctl(id2, IPC_RMID, NULL); // mark ptr to be destroy from the share memory
+                shmctl(id1, IPC_RMID, NULL); // mark ptr to be destroy from the share memory
+                shmctl(id3, IPC_RMID, NULL);
+                shmctl(idFlag2, IPC_RMID, NULL); // mark ptr to be destroy from the share memory
+                shmctl(idFlag1, IPC_RMID, NULL); // mark ptr to be destroy from the share memory
+                shmctl(idFlag3, IPC_RMID, NULL);
+            }
         }
     }
     times(&end);
     getrusage(RUSAGE_SELF, &rend);
-
+    printf("Result : %d\n", final);
     printf("%lf usec\n", (end.tms_utime + end.tms_stime - start.tms_utime - start.tms_stime) * 1000000.0 / sysconf(_SC_CLK_TCK));
 
     printf("%ld usec\n", (rend.ru_utime.tv_sec - rstart.ru_utime.tv_sec) * 1000000 + (rend.ru_utime.tv_usec - rstart.ru_utime.tv_usec) + (rend.ru_stime.tv_sec - rstart.ru_stime.tv_sec) * 1000000 + (rend.ru_stime.tv_usec - rstart.ru_stime.tv_usec));
@@ -127,4 +141,25 @@ int main(int argc, char **argv)
     printf("Involonter context switch : %ld-%ld=%ld\n", rend.ru_nivcsw, rstart.ru_nivcsw, rend.ru_nivcsw - rstart.ru_nivcsw);
     printf("In : %ld\n",rend.ru_inblock - rstart.ru_inblock);
     printf("Out : %ld\n",rend.ru_oublock - rstart.ru_oublock);
+}
+
+
+int somme(int a, int b)
+{
+    return a+b;
+}
+
+int soustraction(int a, int b)
+{
+    return a-b;
+}
+
+int multiplication(int a, int b)
+{
+    return a*b;
+}
+
+int division(int a, int b)
+{
+    return a/b;
 }
